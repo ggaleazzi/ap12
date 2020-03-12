7d09225018346f18282ba2de6c481c3c97aa9e8e
Gustavo Galeazzi <gustavogaleazzi@gmail.com>
2020-03-12 14:17:14 -0500

    api2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [image, setImage] = useState('');
+    const [text, setText] = useState('');
+      
+    function getImage() {
+      fetch('https://spaceimages.herokuapp.com')
+        .then(response => response.json())
+        .then(data => {setImage(data.hdurl);setText(data.description)});
+    }
+
+    useEffect(() => {
+      getImage();
+    }, []);
+  
+    return (
+      <div className="App">
+        <h1>api2</h1>
+        <img src={image}/>
+        <p>{text}</p>
+      </div>
+
+ );
 }
 
 export default App;
